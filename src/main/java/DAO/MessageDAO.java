54b5fa0a558548fca9ed5aed476f0547ccf8a72b
Willy <willygomezr1@gmail.com>
2025-03-30 06:23:28 +0000

    Working on MessageDao

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,14 +1,107 @@
 package DAO;
 import Util.ConnectionUtil;
 import Model.Message;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
 
 public class MessageDAO {
+
+    public Message createMessage(Message message){
+     
+        Connection connection = null;
+
+    try{
+        connection = ConnectionUtil.getConnection();
+
+        String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) Values(?,?,?)";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){   //retrieves the auto-generated key
+    
+            preparedStatement.setInt(1, message.getPosted_by());
+            preparedStatement.setString(2, message.getMessage_text());
+            preparedStatement.setLong(3, message.getTime_posted_epoch());
+             
+            preparedStatement.executeUpdate();
+            
+          try (ResultSet resultSet = preparedStatement.getGeneratedKeys();){ //automatically closing resources
+
+              if(resultSet.next()){
+              int message_ID = resultSet.getInt(1);
+              
+              return new Message(message_ID, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+              
+              }
+          }
+       } 
+        
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+        return null;
+    }
+
+    public List<Message> getAllMessages(){
+
+        Connection connection = null;
+        List<Message> messages = new ArrayList<>();
+        try{
+            connection = ConnectionUtil.getConnection();
+
+            String sql = "SELECT * FROM message";
+
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+              
+               try(ResultSet resultSet = preparedStatement.executeQuery();){
+
+                while(resultSet.next()){
+                    Message message = new Message(resultSet.getInt("message_id"), resultSet.getInt("posted_by"),
+                    resultSet.getString("message_text"), resultSet.getLong("time_posted_epoch"));
+                    messages.add(message);
+                }
+
+            }
+         }
+        } 
+        catch (SQLException e){
+            e.printStackTrace();
+
+        }
+        return messages;
+
+}
+    public Message getMessageByID(Message message){
+
+        Connection connection = null;
+      
+        try{
+            connection = ConnectionUtil.getConnection();
+
+            String sql = "SELECT * FROM message WHERE message_id = ?";
+
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+                  preparedStatement.setInt(1, message.getMessage_id());
+
+               try(ResultSet resultSet = preparedStatement.executeQuery();){
+
+                if(resultSet.next()){ //if since we are only expecting one row
+            
+                    return new Message(resultSet.getInt("message_id"), resultSet.getInt("posted_by"),
+                    resultSet.getString("message_text"), resultSet.getLong("time_posted_epoch"));
+        
+                }
+
+              }
+           }
+        } 
+        catch (SQLException e){
+            e.printStackTrace();
+
+        }
+        return null;
+
+}
     
 }
