eb2e031620bf0d0d633de402f38e11c65dac4502
Willy <willygomezr1@gmail.com>
2025-03-30 18:11:03 +0000

    completed message dao

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,6 +1,7 @@
 package DAO;
 import Util.ConnectionUtil;
 import Model.Message;
+import Model.Account;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
@@ -69,7 +70,6 @@ public class MessageDAO {
 
         }
         return messages;
-
 }
     public Message getMessageByID(Message message){
 
@@ -104,4 +104,106 @@ public class MessageDAO {
 
 }
     
+
+
+public Message deleteMessageByID(Message message){
+
+    Connection connection = null;
+  
+    try{
+        connection = ConnectionUtil.getConnection();
+
+        String sql = "DELETE * FROM message WHERE message_id = ?";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+              preparedStatement.setInt(1, message.getMessage_id());
+
+           try(ResultSet resultSet = preparedStatement.executeQuery();){
+
+            if(resultSet.next()){ 
+        
+                return new Message(resultSet.getInt("message_id"), resultSet.getInt("posted_by"),
+                resultSet.getString("message_text"), resultSet.getLong("time_posted_epoch"));
+    
+            }
+
+          }
+       }
+    } 
+    catch (SQLException e){
+        e.printStackTrace();
+
+    }
+    return null;
+
 }
+
+public Message updateMessageByID(Message message){
+
+    Connection connection = null;
+  
+    try{
+        connection = ConnectionUtil.getConnection();
+
+        String sql = "UPDATE message SET message_text = ? WHERE message_id = ?";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+              preparedStatement.setString(1, message.getMessage_text());
+              preparedStatement.setInt(2, message.getMessage_id());
+
+           try(ResultSet resultSet = preparedStatement.executeQuery();){
+
+            if(resultSet.next()){ 
+        
+                return new Message(resultSet.getInt("message_id"), resultSet.getInt("posted_by"),
+                resultSet.getString("message_text"), resultSet.getLong("time_posted_epoch"));
+    
+            }
+
+          }
+       }
+    } 
+    catch (SQLException e){
+        e.printStackTrace();
+
+    }
+    return null;
+
+}
+
+public List<Message> getAllMessagesFromUser(Account account){
+
+    Connection connection = null;
+    List<Message> messages = new ArrayList<>();
+    try{
+        connection = ConnectionUtil.getConnection();
+
+        String sql = "SELECT * FROM message INNER JOIN account ON account.account_id = message.posted_by WHERE account.account_id = ?";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            
+            preparedStatement.setInt(1, account.getAccount_id());
+          
+            try(ResultSet resultSet = preparedStatement.executeQuery();){
+
+            while(resultSet.next()){
+                Message message = new Message(resultSet.getInt("message_id"), resultSet.getInt("posted_by"),
+                resultSet.getString("message_text"), resultSet.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+
+        }
+     }
+    } 
+    catch (SQLException e){
+        e.printStackTrace();
+
+    }
+    return messages;
+
+}
+}
+
+
